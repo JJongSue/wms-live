b77e0fbdbc6edfd74e221df02c1c68af2206b52e
json_p <json.p@kakaoenterprise.com>
2024-03-26 22:03:31 +0900

    Initial commit

---
+++ src/test/java/org/example/wmslive/WmsLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wmslive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
